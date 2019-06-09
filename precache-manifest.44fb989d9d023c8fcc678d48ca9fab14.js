0b0236b0655dadcbd83cd3a7b17b3494dbbf1b8a
Vikhyat Midha <vikhyatmidha5@gmail.com>
2019-06-09 19:20:37 +0530

    Updates

---
+++ precache-manifest.44fb989d9d023c8fcc678d48ca9fab14.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac0221ac2e9b8c76f266407867b76f2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27369b25f7718ac4d1da",
+    "url": "/static/css/main.67887bc6.chunk.css"
+  },
+  {
+    "revision": "6846dc3454bef3f73281",
+    "url": "/static/js/2.faec3d6b.chunk.js"
+  },
+  {
+    "revision": "27369b25f7718ac4d1da",
+    "url": "/static/js/main.76be7db4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
